f915ff5b6517cfc750646ead6813193ee72ad9bf
azat222 <azat222@gmail.com>
2025-02-17 23:57:49 +0300

    init backend

---
+++ src/main/java/ru/itmo/lab/controllers/HelloWorldController.java
@@ -0,0 +1,18 @@
+package ru.itmo.lab.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@Tag(name = "Hello World Controller", description = "Тестовый контроллер")
+public class HelloWorldController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
