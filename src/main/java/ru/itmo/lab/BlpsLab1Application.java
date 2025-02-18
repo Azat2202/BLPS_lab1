5a06938435fe611193b5218f27b126d959e48ce5
azat222 <azat222@gmail.com>
2025-02-19 01:13:25 +0300

    add swagger

--- src/main/java/ru/itmo/lab/BlpsLab1Application.java
+++ src/main/java/ru/itmo/lab/BlpsLab1Application.java
@@ -1,9 +1,11 @@
 package ru.itmo.lab;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class BlpsLab1Application {
 
     public static void main(String[] args) {
