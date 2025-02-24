d7257456d6aad0ee334e7b38c44d0fc99c092f34
Kostya <kostikbushmelev@gmail.com>
2025-02-25 02:08:22 +0300

    add creating payment

---
+++ src/main/java/ru/itmo/lab/dto/responses/PaymentResponseDTO.java
@@ -0,0 +1,18 @@
+package ru.itmo.lab.dto.responses;
+
+import lombok.Data;
+import ru.itmo.lab.models.Room;
+import ru.itmo.lab.models.User;
+import ru.itmo.lab.models.enums.BookingStatus;
+import ru.itmo.lab.models.enums.PaymentStatus;
+
+import java.time.LocalDate;
+
+@Data
+public class PaymentResponseDTO {
+	private Long id;
+	private User user;
+	private Integer amount;
+	private PaymentStatus status;
+	private LocalDate date;
+}
