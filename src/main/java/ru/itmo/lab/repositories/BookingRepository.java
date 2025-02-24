e920def87308c1a9a8ccab839c8fc0b6bc3ecf1d
Kostya <kostikbushmelev@gmail.com>
2025-02-25 01:53:14 +0300

    add unavailable check for rooms

--- src/main/java/ru/itmo/lab/repositories/BookingRepository.java
+++ src/main/java/ru/itmo/lab/repositories/BookingRepository.java
@@ -3,8 +3,14 @@ package ru.itmo.lab.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.itmo.lab.models.Booking;
+import ru.itmo.lab.models.Room;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+	
+	List<Booking> findAllByRoomAndStartDateLessThanEqualAndEndDateGreaterThanEqual(Room room, LocalDate startDate, LocalDate endDate);
 
 }
