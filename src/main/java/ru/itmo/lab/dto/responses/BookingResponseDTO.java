418655b39dee0b8fea1db2efaf3cf0c92a3e05b3
Kostya <kostikbushmelev@gmail.com>
2025-02-25 00:38:22 +0300

    add interval of dates in booking

--- src/main/java/ru/itmo/lab/dto/responses/BookingResponseDTO.java
+++ src/main/java/ru/itmo/lab/dto/responses/BookingResponseDTO.java
@@ -5,11 +5,14 @@ import ru.itmo.lab.models.Room;
 import ru.itmo.lab.models.User;
 import ru.itmo.lab.models.enums.BookingStatus;
 
+import java.time.LocalDate;
+
 @Data
 public class BookingResponseDTO {
 	private Long id;
 	private User user;
 	private Room room;
 	private BookingStatus status;
-	private String date;
+	private LocalDate startDate;
+	private LocalDate endDate;
 }
