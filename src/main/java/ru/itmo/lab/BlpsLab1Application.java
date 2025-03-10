f5767f81df086c840592b566287fe1642235e6b5
azat222 <azat222@gmail.com>
2025-03-11 02:36:48 +0300

    init atomikos but dont work

--- src/main/java/ru/itmo/lab/BlpsLab1Application.java
+++ src/main/java/ru/itmo/lab/BlpsLab1Application.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @OpenAPIDefinition
 public class BlpsLab1Application {
 	public static void main(String[] args) {
--- src/main/java/ru/itmo/lab/repositories/booking/BookingRepository.java
+++ src/main/java/ru/itmo/lab/repositories/booking/BookingRepository.java
@@ -1,4 +1,4 @@
-package ru.itmo.lab.repositories;
+package ru.itmo.lab.repositories.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
