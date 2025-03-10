bb1201e384f85ffdad0484c46665f0649d263f87
Kostya <kostikbushmelev@gmail.com>
2025-03-11 02:49:55 +0300

    upd privileges

--- src/main/java/ru/itmo/lab/models/enums/Role.java
+++ src/main/java/ru/itmo/lab/models/enums/Role.java
@@ -5,11 +5,11 @@ import java.util.Set;
 public enum Role {
 	USER(Set.of(Privilege.VIEW_HOTELS, Privilege.BOOK_ROOM)),
 	
-	HOTEL_ADMIN(Set.of(Privilege.VIEW_RESERVATIONS)),
+	HOTEL_ADMIN(Set.of(Privilege.VIEW_RESERVATIONS, Privilege.BOOK_ROOM, Privilege.VIEW_HOTELS)),
 	
-	HOTEL_OWNER(Set.of(Privilege.CREATE_ROOM, Privilege.DELETE_ROOM)),
+	HOTEL_OWNER(Set.of(Privilege.CREATE_ROOM, Privilege.DELETE_ROOM, Privilege.BOOK_ROOM, Privilege.VIEW_HOTELS)),
 	
-	SYSTEM_ADMIN(Set.of(Privilege.CREATE_HOTEL, Privilege.CANCEL_BOOKING)),
+	SYSTEM_ADMIN(Set.of(Privilege.CREATE_HOTEL, Privilege.CANCEL_BOOKING, Privilege.VIEW_RESERVATIONS, Privilege.CREATE_ROOM, Privilege.DELETE_ROOM, Privilege.BOOK_ROOM, Privilege.VIEW_HOTELS)),
 	
 	TRANSACTION_APPROVER(Set.of(Privilege.APPROVE_TRANSACTION));
 	
