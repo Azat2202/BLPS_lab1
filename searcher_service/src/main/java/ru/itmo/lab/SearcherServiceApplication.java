10202aa1b5fc3a2373579ee00ef081952255b6a5
azat222 <azat222@gmail.com>
2025-05-19 03:29:33 +0300

    created multimodule maven project

--- searcher_service/src/main/java/ru/itmo/lab/SearcherServiceApplication.java
+++ searcher_service/src/main/java/ru/itmo/lab/SearcherServiceApplication.java
@@ -3,8 +3,6 @@ package ru.itmo.lab;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
@@ -12,8 +10,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableTransactionManagement
 @OpenAPIDefinition
 @EnableMethodSecurity
-public class BlpsLab1Application {
+public class SearcherServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BlpsLab1Application.class, args);
+		SpringApplication.run(SearcherServiceApplication.class, args);
 	}
 }
